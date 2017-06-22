8ae75d1e1564f738ea1bfd47543cf4be7a11596f
Evgeniy Marthenov <evgeniy.marthenov@gmail.com>
2017-06-22 05:31:40 +0300

    edit readme.md, delete hello.vue, delete router

--- src/main.js
+++ src/main.js
@@ -2,17 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import VTooltip from 'v-tooltip'
 import chart from './components/Github-Chart'
 
 Vue.config.productionTip = false;
 Vue.use(VTooltip)
 Vue.component('chart', chart)
-/* eslint-disable no-new */
+
 new Vue({
   el: '#app',
-  router,
   template: '<App/>',
   components: { App }
 })
